d55788e7e1ea297e0cbe27a707b9aeab47dcd191
Krieg <krieggyy1986@gmail.com>
2025-01-20 13:15:21 -0500

    Module3Part2

--- OneDrive/Documents/db-capstone-project/stored_procedure_cancel_order.sql
+++ OneDrive/Documents/db-capstone-project/stored_procedure_cancel_order.sql
@@ -2,5 +2,5 @@ DELIMITER //
 CREATE PROCEDURE CancelOrder(IN order_id INT)
 BEGIN 
 DELETE FROM Orders WHERE OrderID = order_id;
-SELECT * FROM Orders;
+SELECT CONCAT("Order ", order_id," is cancelled") AS Confirmation;
 END //
